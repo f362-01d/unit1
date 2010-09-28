a5e0b4db005446583ad57d5ef7206e084c7fd7d4
Greg Chambers <grg.sox@gmail.com>
2010-09-28 19:50:39 -0400

    Started canvas implementation with getSelected()

--- Draw/src/draw/Canvas.java
+++ Draw/src/draw/Canvas.java
@@ -4,7 +4,31 @@ import java.util.*;
 
 public class Canvas extends Observable {
 	
-	private ArrayList<DrawingPrimitive> primitives = new ArrayList<DrawingPrimitive>();
+    private ArrayList<DrawingPrimitive> primitives;
+
+    public Canvas() {
+
+        primitives = new ArrayList<DrawingPrimitive>();
+
+    } // Canvas constructor
 	
+    public DrawingPrimitive[] getSelected() {
+
+        ArrayList<DrawingPrimitive> selected =
+            new ArrayList<DrawingPrimitive>();
+
+        for(i=0;i<primitives.size;i++){
+            
+            if( primitives.get(i).isSelected ){
+
+                selected.add( primitives.get(i) );
+
+            } // if
+
+        } // for
+
+        return selected.toArray();
+
+    } // getSelected()
 
 }
