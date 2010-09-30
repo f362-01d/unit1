b57f79c3f24a2f91c71cd99a0286b6df685a1475
Samuel Nelson <son9673@rit.edu>
2010-09-30 01:27:31 -0400

    I wanna keep my draw debug

--- Draw/src/draw/Draw.java
+++ Draw/src/draw/Draw.java
@@ -34,17 +34,26 @@ public class Draw extends JFrame {
 			Canvas c = new Canvas();
 			
 			{
-				Create m = new Create(this, c, new Rectangle(null, null).getClass());
+				Select m = new Select(this, c);
+				//Create cr = new Create(this, c, new Line(null, null).getClass());
+				this.setFocusable(true);
+				this.requestFocus();
+				//addMouseListener(cr);
 				addMouseListener(m);
-				addMouseMotionListener(m);
-				c.getElements().add(new Rectangle(new Point(5, 5), new Point(200, 200)));
-				c.getElements().add(new Line(new Point(20,20), new Point(80,40)));
+				addKeyListener(m);
+				//addMouseMotionListener(cr);
+				c.getPrimitives().add(new Rectangle(new Point(5, 5), new Point(200, 200)));
+				c.getPrimitives().add(new Line(new Point(20,20), new Point(80,40)));
+				c.getPrimitives().add(new Rectangle(new Point(400, 400), new Point(600, 450)));
+				c.getPrimitives().add(new Rectangle(new Point(250, 400), new Point(50, 450)));
+				c.getPrimitives().add(new Line(new Point(100,20), new Point(300,0)));
+				c.getPrimitives().add(new Line(new Point(20,205), new Point(300,205)));
 			}
 			
 			public void paintComponent(Graphics g)
 			{
 				g.clearRect(0, 0, getWidth(), getHeight());
-				ArrayList<DrawingPrimitive> elements = c.getElements();
+				ArrayList<DrawingPrimitive> elements = c.getPrimitives();
 				for(int i=0;i<elements.size();i++)
 					elements.get(i).draw((Graphics2D)g);
 			}
