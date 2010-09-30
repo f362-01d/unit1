cbf791f56cd0bacd972a47b20ef7e0f960b4b9ab
Rick <rxj9246@rit.edu>
2010-09-29 23:23:17 -0400

    Updating lots of files

--- Draw/src/draw/TextBuilder.java
+++ Draw/src/draw/TextBuilder.java
@@ -5,29 +5,52 @@ import java.io.*;
 
 public class TextBuilder extends FileBuilder {
 	
+	public TextBuilder() {
+		super();
+	}
+	
 	public void save(ArrayList<DrawingPrimitive> a, String n) {
 		
+		BufferedWriter saver = null;
+		
 		try {
-			FileWriter saver = new FileWriter(n);
+			saver = new BufferedWriter(new FileWriter(n));
 		} catch (IOException e) {
 			System.err.println("Could not write file.");
 		}
 		
 		ArrayList<String> fileLines;
 		TextFormat format = new TextFormat(a);
-		fileLines = format.makeShapeList();
+		fileLines = format.makeStringList();
 		ListIterator<String> lineIter = fileLines.listIterator();
 		try {
 			while (lineIter.hasNext()) {
 				String line = lineIter.next();
-				
+				saver.write(line);
+				saver.newLine();
 			}
+			saver.close();
 		} catch (IOException ex) {
 			System.err.println("Error writing file.");
 		}
 	}
 	
 	public ArrayList<DrawingPrimitive> load(String f) {
-		return null;
+		BufferedReader reader = null;
+		ArrayList<String> lines = new ArrayList<String>();
+		TextFormat shapeFormat = new TextFormat(
+						new ArrayList<DrawingPrimitive>());
+		try {
+			reader = new BufferedReader(new FileReader(f));
+			String line = new String();
+			line = reader.readLine();
+			while (!line.equals(null)) {
+				lines.add(line);
+			}
+			reader.close();
+		} catch(IOException e) {
+			System.err.println("Error loading file.");
+		}
+		return shapeFormat.makeShapes(lines);
 	}
 }
--- Draw/src/draw/TextFormat.java
+++ Draw/src/draw/TextFormat.java
@@ -8,11 +8,71 @@ public class TextFormat extends FileFormat {
 		super(s);
 	}
 	
-	public ArrayList<String> makeShapeList() {
+	public ArrayList<String> makeStringList() {
+		ListIterator<DrawingPrimitive> shapeItr = this.shapes.listIterator();
+		
+		while (shapeItr.hasNext()) {
+			DrawingPrimitive currentShape = shapeItr.next();
+			if (currentShape.getClass().toString().equals("Line")) {
+				makeLLines(this.shapeLines, currentShape);
+			} else if (currentShape.getClass().toString().equals("Rectangle")) {
+				makeRLines(this.shapeLines, currentShape);
+			} else {
+				makeGLines(this.shapeLines, currentShape);
+			}
+		}
+		
+		return this.shapeLines;
+	}
+	
+	public ArrayList<DrawingPrimitive> makeShapes(ArrayList<String> l) {
 		return null;
 	}
 	
-	public ArrayList<DrawingPrimitive> makeShapes() {
-		return null;
+	protected void makeLLines(ArrayList<String> l,
+								DrawingPrimitive p) {
+		String line = new String("line ");
+		line.concat(p.getPosition().getX() + " ");
+		line.concat(p.getPosition().getY() + " ");
+		line.concat((p.getPosition().getX()
+				+ p.getSize().getWidth()) + " ");
+		line.concat((p.getPosition().getY()
+				+ p.getSize().getHeight()) + " ");
+		line.concat(p.getColor().toString());
+		l.add(line);
+	}
+	
+	protected void makeRLines(ArrayList<String> l,
+								DrawingPrimitive p) {
+		String line = new String("rect ");
+		line.concat(p.getPosition().getX() + " ");
+		line.concat(p.getPosition().getY() + " ");
+		line.concat((p.getPosition().getX()
+				+ p.getSize().getWidth()) + " ");
+		line.concat((p.getPosition().getY()
+				+ p.getSize().getHeight()) + " ");
+		line.concat(p.getColor().toString());
+		l.add(line);
+	}
+	
+	protected void makeGLines(ArrayList<String> l,
+								DrawingPrimitive p) {
+		l.add("begin");
+		
+		ListIterator<DrawingPrimitive> elementItr = p.getElements()
+													.listIterator();
+		
+		while (elementItr.hasNext()) {
+			DrawingPrimitive currentElement = elementItr.next();
+			if (currentElement.getClass().toString().equals("Line")) {
+				makeLLines(l, currentElement);
+			} else if (currentElement.getClass().toString().equals("Rectangle")) {
+				makeRLines(l, currentElement);
+			} else {
+				makeGLines(l, currentElement);
+			}
+		}
+		
+		l.add("end");
 	}
 }
