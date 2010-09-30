26987503284b110dcbe7b2a2059afde800eab389
Greg Chambers <grg.sox@gmail.com>
2010-09-29 21:11:10 -0400

    Added add and overloaded remove methods to Canvas.

--- Draw/src/draw/Canvas.java
+++ Draw/src/draw/Canvas.java
@@ -17,18 +17,36 @@ public class Canvas extends Observable {
         ArrayList<DrawingPrimitive> selected =
             new ArrayList<DrawingPrimitive>();
 
+        // Iterate over the array of DrawingPrimitives, adding all selected
+        // elements to another array which is later returned.
         for(i=0;i<primitives.size;i++){
-            
             if( primitives.get(i).isSelected ){
 
                 selected.add( primitives.get(i) );
 
             } // if
-
         } // for
 
         return selected.toArray();
 
     } // getSelected()
 
-}
+    public void add( DrawingPrimative newPrimitive ){
+
+        primitives.add( newPrimitive );
+
+    } // add()
+
+    public void remove( DrawingPrimitive killPrimitive ){
+
+        primitives.remove( killPrimitive );
+
+    } // remove()
+
+    public void remove( int killIndex ){
+
+        primitives.remove( killIndex );
+
+    } // remove()
+
+} // Canvas
